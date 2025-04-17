6a00dcc0031783b02033c99487b7ba81d688e271
jhllolia <jhllolia@naver.com>
2025-04-17 20:15:54 +0900

    수정

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,1 +1,11 @@
 from db.connection import get_connection
+
+with get_connection() as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("""
+            ALTER USER 'tosokcokr'@'%' 
+            IDENTIFIED WITH mysql_native_password BY 'tosokcokr##^^';
+        """)
+        cursor.execute("FLUSH PRIVILEGES;")
+        cursor.close()
+    conn.commit()
